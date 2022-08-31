09b17cde997e40c54aadea99c8845705be0d2694
HoneyCreator <honeypot.creator@gmail.com>
2022-09-01 00:49:08 +0900

    profile tab

--- src/components/profileMenu.tsx
+++ src/components/profileMenu.tsx
@@ -1,45 +1,16 @@
 import { useNavigate } from "react-router-dom"
+import '../assets/styles/profile/components/profileMenuStyle.scss'
 
 const ProfileMenu = (props: any) => {
   const { url } = props
   const navigate = useNavigate()
-
-  const style = {
-    profileMenu: {
-      display: "flex",
-      flexDirection: "row" as const,
-      justifyContent: "space-between",
-      background: "#FFFFFF",
-      boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.1)",
-      borderRadius: "10px",
-      padding: "10px 55px",
-    },
-    dareme: {
-      color: props.menu === "dareme" ? "#EFA058" : "#D6D5CC",
-      fontStyle: "normal",
-      fontWeight: "bold",
-      fontSize: "18px",
-      lineHeight: "20px",
-      cursor: "pointer",
-      display: "flex",
-      justifyContent: 'center',
-    },
-
-    fanWall: {
-      color: props.menu === "dareme" ? "#D6D5CC" : "#EFA058",
-      fontStyle: "normal",
-      fontWeight: "bold",
-      fontSize: "18px",
-      lineHeight: "20px",
-      cursor: "pointer",
-    },
-  };
+  
   return (
-    <div style={style.profileMenu}>
-      <div style={style.dareme} onClick={() => { navigate(`/${url}`) }}>
+    <div className="profile-menu-wrapper">
+      <div className={props.menu === "dareme" ? "dareme-fanwall-active" : 'dareme-fanwall'} onClick={() => { navigate(`/${url}`) }}>
         DareMe & FundMe
       </div>
-      <div style={style.fanWall} onClick={() => { navigate(`/${url}/fanwall`) }}>
+      <div className={props.menu === "dareme" ? "dareme-fanwall" : 'dareme-fanwall-active'} onClick={() => { navigate(`/${url}/fanwall`) }}>
         Fan Wall
       </div>
     </div>
